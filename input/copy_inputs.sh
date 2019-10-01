3eeb4471b5a86b8bacecddb9a1e303ffa0bac48a
Anastasia Karavdina <anastasia.karavdina@desy.de>
2019-10-01 10:43:09 +0200

    running code for BNN

--- input/copy_inputs.sh
+++ input/copy_inputs.sh
@@ -1,1 +1,3 @@
-scp -r naf23:/nfs/dust/cms/user/reimersa/ZprimeSemiLeptonic/94X_v1/Fullselection/2017_Moriond19JEC_RightLumiweights_forml_Puppi/NOMINAL/MLInput .
+#scp -r karavdia@naf-cms12.desy.de:/nfs/dust/cms/user/reimersa/ZprimeSemiLeptonic/94X_v1/Fullselection/2017_Moriond19JEC_RightLumiweights_forml_Puppi/NOMINAL/MLInput .
+
+#scp -r karavdia@naf-cms12.desy.de:/beegfs/desy/user/reimersa/ZprimeClassifier/input/classes_4_QCD_Mu_TTbar+ST_DYJets+WJets_RSGluon_All/ .
